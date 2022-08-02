851207f6ee278aab5bb6c8fcd842d2506cab2e10
dvke <uchennaobiekwe@yahoo.com>
2022-08-02 14:57:39 +0100

    add function to get computer choice

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice(){
+    // store options in array
+    const options = ['rock','paper','scissors'];
+
+    // return random option
+    return options[Math.floor(Math.random()*3)];
+}
+
+
+
+
+
+
+
+
+
