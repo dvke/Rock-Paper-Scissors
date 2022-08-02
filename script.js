cf3d45c1f8ac11641fbbf4cb24923e0a482aa6d6
dvke <uchennaobiekwe@yahoo.com>
2022-08-02 14:58:40 +0100

    add function to play a round of rock-paper-scissors

--- script.js
+++ script.js
@@ -6,6 +6,31 @@ function getComputerChoice(){
     return options[Math.floor(Math.random()*3)];
 }
 
+function playRound(playerSelection, computerSelection){
+    // display selection
+    console.log(`Your selection: ${playerSelection}`);
+    console.log(`Computer selection: ${computerSelection}`);
+    // declare outcome as boolean
+    const outcome = true;
+
+    // traverse possible outcomes and return result
+    switch(outcome){
+        case (playerSelection == 'rock' && computerSelection == 'scissors'):
+        case (playerSelection == 'paper' && computerSelection == 'rock'):
+        case (playerSelection == 'scissors' && computerSelection == 'paper'):
+            return 'You win!';
+        case (playerSelection == 'rock' && computerSelection == 'paper'):
+        case (playerSelection == 'paper' && computerSelection == 'scissors'):
+        case (playerSelection == 'scissors' && computerSelection == 'rock'):
+            return 'You Lose!';
+        case(playerSelection == computerSelection):
+            return 'Draw!';
+        default:
+            return 'Invalid Input';
+    }
+
+}
+
 
 
 
