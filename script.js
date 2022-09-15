f88e2d6d3278fbe38a1ece33c6fdf2f2eaad2750
dvke <uchennaobiekwe@yahoo.com>
2022-09-15 14:07:13 +0100

    adding some style.

--- script.js
+++ script.js
@@ -3,12 +3,27 @@ let playerScore = 0;
 let computerScore = 0;
 
 
+const container = document.querySelector('.container');
+const card = document.querySelector('.card');
 const options = document.querySelectorAll('.option');
+const selection = document.getElementById('selection');
+const result = document.getElementById('result');
+const cardBack = document.querySelector('.card__face--back');
+
+
+const playerScoreDisplay = document.getElementById('playerScore');
+const computerScoreDisplay = document.getElementById('computerScore');
+
 
 // create click even for each option
 [...options].forEach((option)=>option.addEventListener('click', (e)=>playRound(e.target.id, getComputerChoice())));
 
 
+function resetGame(){
+    playerScore = 0;
+    computerScore = 0;
+    playerSelection = '';
+}
 
 function getComputerChoice(){
     // store options in array
@@ -19,11 +34,11 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection){
-    // display selection
-    console.log(`Your selection: ${playerSelection}`);
-    console.log(`Computer selection: ${computerSelection}`);
     // declare outcome as boolean
     const outcome = true;
+            
+    // display selection
+    selection.innerHTML = `<p>Your selection: ${playerSelection}</p><p>Computer selection: ${computerSelection}</p>`;
 
     // traverse possible outcomes and return result
     switch(outcome){
@@ -31,48 +46,51 @@ function playRound(playerSelection, computerSelection){
         case (playerSelection == 'paper' && computerSelection == 'rock'):
         case (playerSelection == 'scissors' && computerSelection == 'paper'):
             playerScore+=1;
-            console.log ('You win this round!');
+            result.innerHTML = '<p style="color:green;">Result: You win this round!</p>';
             break;
         case (playerSelection == 'rock' && computerSelection == 'paper'):
         case (playerSelection == 'paper' && computerSelection == 'scissors'):
         case (playerSelection == 'scissors' && computerSelection == 'rock'):
             computerScore+=1;
-            console.log('You Lost this round!');
+            result.innerHTML = '<p style="color:red;">Result: You lost this round!</p>';
             break;
         case(playerSelection == computerSelection):
-            console.log('Draw!');
+            result.innerHTML = '<p>Result: Draw!</p>';
             break;
         default:
-            console.log('Invalid Input');
+            result.innerHTML = '<p>Result: Invalid!</p>';
             break;
     }
 
-}
+        playerScoreDisplay.innerHTML = `<p>Your score = ${playerScore}</p>`;
+        computerScoreDisplay.innerHTML = `<p>Computer score = ${computerScore}</p>`;
 
-function playGame(){
-    alert('Welcome to Rock-Paper-Scissors');
-    while(playerScore < 5 || computerScore < 5){
-        playerSelection = prompt('Enter your selection: ');
-        if(playerSelection==null){
-            console.log('Game cancelled');
-            break;   
-        }
-        console.log(playRound(playerSelection,getComputerChoice()));
-        console.log(`Your score = ${playerScore}`);
-        console.log(`Computer score = ${computerScore}`);
 
+        // end game if score of 5 is reached
         if(computerScore === 5){
-            console.log('You lost the game!');
-            break;
+            cardBack.setAttribute('style','display:block;');
+            card.classList.toggle('is-flipped');
+            document.getElementById('winOrLose').textContent = 'You Lose!';
+            resetGame();
         }
         else if(playerScore === 5){
-            console.log('You won the game!');
-            break;
+            cardBack.setAttribute('style','display:block;');
+            card.classList.toggle('is-flipped');
+            document.getElementById('winOrLose').textContent = 'You Win!';
+            resetGame();
         }
-    }
-
-   
 }
+   
+
+// New game function which resets the variables.
+  document.getElementById('newGame').addEventListener( 'click', function() {
+    selection.innerHTML = '';
+    result.innerHTML = '';
+    cardBack.setAttribute('style','display:none;');
+    playerScoreDisplay.innerHTML = '';
+    computerScoreDisplay.innerHTML = '';
+    card.classList.toggle('is-flipped');
+  });
 
 
 
